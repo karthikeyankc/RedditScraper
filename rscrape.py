f8bf38994a5df317d66d1cdd81d6cc8fcccd62ac
Karthikeyan KC <karthikeyankc@live.com>
2016-08-24 12:18:24 +0530

    revisions.

--- rscrape.py
+++ rscrape.py
@@ -19,43 +19,32 @@ def scrape(subreddit_name):
 		html = s.get(url)
 		content = html.content
 		j_dict = json.loads(content)
-
 		after = j_dict["data"]["after"]
-
 		children = j_dict["data"]["children"] # list
-
 		if children:
-
 			children_data = []
-
 			for x in children:
 				children_data = x["data"]
 				result = children_data["title"]
 				content_list.append(result)
-
 			if after == None:
 				print "Scraping has reached the final page or hit the API limit of 1000!"
 			else:
 				print "Next page found at %s.\n" %after
 		else:
 			break
-
 	return content_list
 
 def textify(content_list):
-	
 	common_identifiers = ["LPT", "[LPT]", "TIL"]
 	ignore_tags = ["Mod Request", "LPT Request"]
-
 	for content in content_list:
-		
 		if any(ignore_tag in content for ignore_tag in ignore_tags):
 			for tag in ignore_tags:
 				if tag == "Mod Request" and tag in content:
 					content_list.remove(content)
 				elif tag == "LPT Request" and tag in content:
 					content_list.remove(content)
-		
 		elif any(identifier in content for identifier in common_identifiers):
 			for identifier in common_identifiers:
 				if identifier == "TIL" and identifier in content:
@@ -68,11 +57,9 @@ def textify(content_list):
 					break
 				else:
 					continue
-
 		else:
 			with open("scraped contents from "+subreddit+".txt", 'a+') as text_file:
 				text_file.write(content.encode('utf-8')+"\n")
-		
 	print "Scraping successful! Contents are written to 'Scraped Contents from "+subreddit+".txt'!"
 
 def write(new):
